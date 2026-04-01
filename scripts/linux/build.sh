ca40e7492bf93c6ef5648c7b3ed980f6bcf5a849
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-02 00:05:23 +0500

    patch

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-git clone https://github.com/TauricResearch/TradingAgents.git temp
+git clone --depth 1 https://github.com/TauricResearch/TradingAgents.git temp
+rm -rf temp/.git
 rm -rf ./src/agent
 cp -r temp ./src/agent
 rm -rf temp
--- scripts/win/build.bat
+++ scripts/win/build.bat
@@ -1,5 +1,6 @@
 @echo off
-call git clone https://github.com/TauricResearch/TradingAgents.git temp
+call git clone --depth 1 https://github.com/TauricResearch/TradingAgents.git temp
+rd /s /q temp\.git
 rd /s /q src\agent
 xcopy /e /i /h /q temp src\agent
 rd /s /q temp
