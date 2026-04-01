2f419036bf6fb0f8aac200fd31d8c26f06b5c194
Petr Tripolsky <tripolskypetr@gmail.com>
2026-04-02 00:07:39 +0500

    keep

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -2,5 +2,7 @@
 git clone --depth 1 https://github.com/TauricResearch/TradingAgents.git temp
 rm -rf temp/.git
 rm -rf ./src/agent
-cp -r temp ./src/agent
+mkdir -p ./src/agent
+touch ./src/agent/.gitkeep
+cp -r temp/. ./src/agent
 rm -rf temp
--- scripts/win/build.bat
+++ scripts/win/build.bat
@@ -2,5 +2,7 @@
 call git clone --depth 1 https://github.com/TauricResearch/TradingAgents.git temp
 rd /s /q temp\.git
 rd /s /q src\agent
+mkdir src\agent
+type nul > src\agent\.gitkeep
 xcopy /e /i /h /q temp src\agent
 rd /s /q temp
